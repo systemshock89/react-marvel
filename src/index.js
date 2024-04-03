597becfb2bd097c60c38bdd689ff0e2a51c1bd55
systemshock89 <666qqq77>
2024-04-03 17:51:58 +0300

    upd to react 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // for react 18
+// import ReactDOM from 'react-dom'; // for react 17
 import App from './components/app/App';
 import './style/style.scss';
+import { flushSync } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// for react 18
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+// for react 17
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
